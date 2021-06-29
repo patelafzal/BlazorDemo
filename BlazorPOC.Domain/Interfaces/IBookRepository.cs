0c161a0523bb7d6f2f1df53c22801ca74d375041
Bhumi Parekh <bhumiparekh02@gmail.com>
2021-06-29 11:43:16 +0530

    I have added XML comment on IBookRepository

--- BlazorPOC.Domain/Interfaces/IBookRepository.cs
+++ BlazorPOC.Domain/Interfaces/IBookRepository.cs
@@ -5,6 +5,10 @@ namespace BlazorPOC.Domain.Interfaces
 {
     public interface IBookRepository : IRepositoryBase<Book>
     {
+        /// <summary>
+        /// This function is useful for adding book data
+        /// </summary>
+        /// <param name="model"></param>
         void AddBook(BookModel model);
     }
 }
